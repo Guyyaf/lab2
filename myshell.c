3e8133a97f470ac039d71d5a78dfb6879acad7ab
Guy <guyyaffe99@gmail.com>
2024-05-29 19:50:33 +0300

    add

---
+++ myshell.c
@@ -0,0 +1,34 @@
+#include "LineParser.h"
+#include "LineParser.c"
+#include <limits.h>
+#include <unistd.h>
+
+#define MAX_INPUT 2048 
+
+void execute(cmdLine *pCmdLine) {
+    int pid;
+    if (pid = fork() == 0) { 
+        if (execv(pCmdLine->arguments[0], pCmdLine->arguments) == -1) {
+            perror("execvp failed");
+            exit(1);
+        }
+    } else { 
+        waitforchild(pid);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    char cwdBuffer[PATH_MAX];
+    char inputBuffer[MAX_INPUT];
+    cmdLine* input;
+    while(1==1){
+        getcwd(cwdBuffer, PATH_MAX);
+        printf("Current working directory is - %s\n", cwdBuffer);
+        fgets(inputBuffer,MAX_INPUT,stdin);
+        input = parseCmdLines(inputBuffer);
+        execute(input);
+        freeCmdLines(input);
+    }
+    return 0;    
+}
+
